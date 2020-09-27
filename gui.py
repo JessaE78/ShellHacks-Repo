0a62d3a846b8a77319656f09b122efa79c9e3834
Bowen Wu <fsdi321@mit.edu>
2020-09-26 23:51:19 -0400

    Rewrote translation implementation, now supports displaying pronunciation of original language

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
 #GUI to ask user what language they speak, so we can translate image text to their language
 from tkinter import *
+from main import translate_image
+from lang_code import *
 import os
 import sys
 import shutil
@@ -9,34 +11,44 @@ window.geometry("500x500")
 window.title("Menu Translator")
 
 
+lang_tuple = []
+for lang in LANGUAGES.values():
+    lang_tuple.append(lang.capitalize())
+lang_tuple = tuple(lang_tuple)
+
+image = None
+lang_code = "en"
+use_pronunciation = BooleanVar()
 
 def UploadAction(event=None):
     from tkinter import filedialog
-    filename = filedialog.askopenfilename()
-    print('Selected:', filename)
-    # os.rename(filename, 'Users\Documents\GitHub\ShellHacks-Repo\resources')
-    destination = os.path.abspath('resources')
-    shutil.move(filename, destination)
+    global image
+    image = filedialog.askopenfile(mode='rb')
+    print('Selected:', image.name)
 
 def Translate():
-    os.system('python main.py')
+    translated_img = translate_image(image, lang_code, use_pronunciation)
 
-def show():
-    myLabel = Label(window, text=clicked.get()).pack()
+def set_lang(lang):
+    lang = lang.lower()
+    global lang_code
+    lang_code = LANGCODES[lang]
+
+def set_use_pronunciation(val):
+    global use_pronunciation
+    use_pronunciation = val
 
 clicked = StringVar()
 clicked.set("English")
-
-drop = OptionMenu(window, clicked, "English", "Spanish", "French", "Italian", "Portuguese", "German", "Russian", "Arabic", "Vietnamese", "Filipino")
+drop = OptionMenu(window, clicked, *lang_tuple, command = set_lang)
 drop.pack()
-
-menuButton = Button(window, text="Show Selection", command=show).pack()
 
 label1 = Label(window, text="Welcome to our Menu Translator!", font=("arial", 16, "bold")).pack()
 label2 = Label(window, text="Please upload a picture of the menu you want translated", font=("arial", 10)).pack()
 
+pronunciation = Checkbutton(window, text="Show Pronunciation Instead", variable=use_pronunciation, onvalue = True, offvalue = False).pack()
+
 buttonUpload = Button(window, text='Open', command=UploadAction).pack()
 button1 = Button(window, text = "Translate", command=Translate).pack()
-# button1.place(x=500, y=110)
 
 window.mainloop()
--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+def check_coords(tl_coord, br_coord): #top left coord and bottom right coord
+    '''
+    Make sure that the top left coordinate is actually top left, and bottom right coord is actually bottom right
+    '''
+    if tl_coord.x > br_coord.x:
+        tl_coord.x, br_coord.x = br_coord.x, tl_coord.x
+    if tl_coord.y > br_coord.y:
+        tl_coord.y, br_coord.y = br_coord.y, tl_coord.y
+    return tl_coord, br_coord
+
+def get_full_text_n_size(block):
+    full_words = ""
+    size = 0
+    for paragraph in block.paragraphs:
+        for word in paragraph.words:
+            size = abs(word.bounding_box.vertices[0].y - word.bounding_box.vertices[2].y)
+            for symbol in word.symbols:
+                full_words+=symbol.text
+                break_type = symbol.property.detected_break.type
+                if break_type == 1:
+                    full_words+=" "
+                elif break_type == 5 or break_type == 3:
+                    full_words+="\n"
+    full_words += "@"
+    return full_words, size
+
 import io
 import os
 import googletrans
@@ -10,61 +36,114 @@ from google.cloud import vision
 from google.cloud.vision import types
 from googletrans import Translator
 
-translator = Translator()
-#sets up GOOGLE_APPLICATION_CREDENTIALS 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'menuTranslatorAuthentication.json'
-# Instantiates a client
-client = vision.ImageAnnotatorClient()
+def translate_image(image_file, dest_lang, do_pronunciation):
+    translator = Translator()
+    #sets up GOOGLE_APPLICATION_CREDENTIALS 
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'menuTranslatorAuthentication.json'
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
 
-# The name of the image file to annotate
-file_name = os.path.abspath('resources/italian_menu_test.jpg')
+    # The name of the image file to annotate
+    #file_name = os.path.abspath(image_path)
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
+    # Loads the image into memory
+    # with io.open(file_name, 'rb') as image_file:
     content = image_file.read()
 
-image = types.Image(content=content)
+    image = types.Image(content=content)
 
-# Performs label detection on the image file
-text_anno = client.text_detection(image=image).text_annotations
-with open('test.txt', 'w', encoding = 'utf-8') as f:
-    f.write(str(text_anno))
-separated_text = text_anno[0].description.split('\n')
-separated_text.pop() #Get rid of last element, which is nothing
+    # Performs label detection on the image file
+    text_anno = client.text_detection(image=image)
+    text_blocks = text_anno.full_text_annotation.pages[0].blocks
+    text_to_translate = ""
+    text_sizes = []
+    for block in text_blocks:
+        text, text_size = get_full_text_n_size(block)
+        text_to_translate+=text
+        text_sizes.append(text_size)
 
-text_index = 1
-#for text in separated_text:
+    translated = translator.translate(text_to_translate, src = text_anno.text_annotations[0].locale, dest = dest_lang)
+    translated_text_separated = translated.text.split('@')
+    translated_text_separated.pop()
+    pronunciation = translated.extra_data["translation"][len(translated.extra_data["translation"]) - 1][3]
+    pronunciation_separated = pronunciation.split('@')
+    pronunciation_separated.pop()
 
-translated = translator.translate(text_anno[0].description, src = text_anno[0].locale, dest = 'es')
-translated_text_separated = translated.text.split('\n')
-image = Image.open(file_name)
-draw = ImageDraw.Draw(image)
-#print(translated_text_separated)
-for i in range(len(separated_text)):
-    #Get the number of words
-    text_num_words = len(separated_text[i].split(' '))
-    #print(text_num_words)
+    image = Image.open(image_file)
+    draw = ImageDraw.Draw(image)
+    for i in range(min(len(translated_text_separated), len(text_blocks))):
+        #Get the verticies of the first and last word of the portion being translated
+        top_left_vert = text_blocks[i].bounding_box.vertices[0]
+        bot_right_vert = text_blocks[i].bounding_box.vertices[2]
+        top_left_vert, bot_right_vert = check_coords(top_left_vert, bot_right_vert)
+        #Crop and blur portion that's being translated
+        cropped_img = image.crop((top_left_vert.x,top_left_vert.y,bot_right_vert.x,bot_right_vert.y))
+        blurred_img = cropped_img.filter(ImageFilter.GaussianBlur(radius=8))
+        image.paste(blurred_img, (top_left_vert.x,top_left_vert.y))
+        print(i)
+        #Set font size to the size of original text
+        font_type = ImageFont.truetype('fonts/ARIALUNI.ttf', text_sizes[i])
+        if do_pronunciation:
+            #Draw outline of text
+            draw.text(xy = (top_left_vert.x - 1, top_left_vert.y), text=pronunciation_separated[i], fill=(0,0,0), font = font_type)
+            draw.text(xy = (top_left_vert.x + 1, top_left_vert.y), text=pronunciation_separated[i], fill=(0,0,0), font = font_type)
+            draw.text(xy = (top_left_vert.x, top_left_vert.y - 1), text=pronunciation_separated[i], fill=(0,0,0), font = font_type)
+            draw.text(xy = (top_left_vert.x, top_left_vert.y + 1), text=pronunciation_separated[i], fill=(0,0,0), font = font_type)
+            #Write translated text
+            draw.text(xy = (top_left_vert.x, top_left_vert.y), text=pronunciation_separated[i], fill=(255,255,255), font = font_type)
+        else:
+            #Draw outline of text
+            draw.text(xy = (top_left_vert.x - 1, top_left_vert.y), text=translated_text_separated[i], fill=(0,0,0), font = font_type)
+            draw.text(xy = (top_left_vert.x + 1, top_left_vert.y), text=translated_text_separated[i], fill=(0,0,0), font = font_type)
+            draw.text(xy = (top_left_vert.x, top_left_vert.y - 1), text=translated_text_separated[i], fill=(0,0,0), font = font_type)
+            draw.text(xy = (top_left_vert.x, top_left_vert.y + 1), text=translated_text_separated[i], fill=(0,0,0), font = font_type)
+            #Write translated text
+            draw.text(xy = (top_left_vert.x, top_left_vert.y), text=translated_text_separated[i], fill=(255,255,255), font = font_type)
+    return image
 
-    #Get the verticies of the first and last word of the portion being translated
-    verts_first_word = text_anno[text_index].bounding_poly.vertices
-    verts_last_word = text_anno[text_index + text_num_words - 1].bounding_poly.vertices
-    #Crop and blur portion that's being translated
-    cropped_img = image.crop((verts_first_word[0].x,verts_first_word[0].y,verts_last_word[2].x,verts_last_word[2].y))
-    #blurred_img = cropped_img.filter(ImageFilter.GaussianBlur(radius=8))
-    image.paste(cropped_img, (verts_first_word[0].x,verts_first_word[0].y))
 
-    #Set font size to the size of original text
-    font_type = ImageFont.truetype('fonts/times.ttf', verts_last_word[2].y - verts_first_word[0].y + 1)
-    #Draw outline of text
-    draw.text(xy = (verts_first_word[0].x - 1, verts_first_word[0].y), text=translated_text_separated[i], fill=(0,0,0), font = font_type)
-    draw.text(xy = (verts_first_word[0].x + 1, verts_first_word[0].y), text=translated_text_separated[i], fill=(0,0,0), font = font_type)
-    draw.text(xy = (verts_first_word[0].x, verts_first_word[0].y - 1), text=translated_text_separated[i], fill=(0,0,0), font = font_type)
-    draw.text(xy = (verts_first_word[0].x, verts_first_word[0].y + 1), text=translated_text_separated[i], fill=(0,0,0), font = font_type)
-    #Write translated text
-    draw.text(xy = (verts_first_word[0].x, verts_first_word[0].y), text=translated_text_separated[i], fill=(255,255,255), font = font_type)
 
-    text_index += text_num_words
 
-print("Finished")
-image.show()
+# # with open('test.txt', 'w', encoding = 'utf-8') as f:
+# #     f.write(str(text_anno.full_text_annotation))
+# text_anno = client.text_detection(image=image).text_annotations
+# separated_text = text_anno[0].description.split('\n')
+# separated_text.pop() #Get rid of last element, which is nothing
 
+# text_index = 1
+# #for text in separated_text:
+
+# translated = translator.translate(text_anno[0].description, src = text_anno[0].locale, dest = dest_lang)
+# print(translated.pronunciation)
+# translated_text_separated = translated.text.split('\n')
+# image = Image.open(file_name)
+# draw = ImageDraw.Draw(image)
+# #print(translated_text_separated)
+# for i in range(len(separated_text)):
+#     #Get the number of words
+#     text_num_words = len(separated_text[i].split(' '))
+#     #print(text_num_words)
+
+#     #Get the verticies of the first and last word of the portion being translated
+#     verts_first_word = text_anno[text_index].bounding_poly.vertices[0]
+#     verts_last_word = text_anno[text_index + text_num_words - 1].bounding_poly.vertices[2]
+#     verts_first_word, verts_last_word = check_coords(verts_first_word, verts_last_word)
+#     #Crop and blur portion that's being translated
+#     cropped_img = image.crop((verts_first_word.x,verts_first_word.y,verts_last_word.x,verts_last_word.y))
+#     blurred_img = cropped_img.filter(ImageFilter.GaussianBlur(radius=8))
+#     image.paste(blurred_img, (verts_first_word.x,verts_first_word.y))
+
+#     #Set font size to the size of original text
+#     font_type = ImageFont.truetype('fonts/times.ttf', verts_last_word.y - verts_first_word.y + 1)
+#     #Draw outline of text
+#     draw.text(xy = (verts_first_word.x - 1, verts_first_word.y), text=translated_text_separated[i], fill=(0,0,0), font = font_type)
+#     draw.text(xy = (verts_first_word.x + 1, verts_first_word.y), text=translated_text_separated[i], fill=(0,0,0), font = font_type)
+#     draw.text(xy = (verts_first_word.x, verts_first_word.y - 1), text=translated_text_separated[i], fill=(0,0,0), font = font_type)
+#     draw.text(xy = (verts_first_word.x, verts_first_word.y + 1), text=translated_text_separated[i], fill=(0,0,0), font = font_type)
+#     #Write translated text
+#     draw.text(xy = (verts_first_word.x, verts_first_word.y), text=translated_text_separated[i], fill=(255,255,255), font = font_type)
+
+#     text_index += text_num_words
+
+# print("Finished")
+# image.show()
